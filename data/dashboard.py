32995e277222bf599f21bfe646d8f3dd2c6cd246
Dan Kerrigan <dankerrigan@basho.com>
2013-12-12 14:27:26 -0500

    initial

--- data/dashboard.py
+++ data/dashboard.py
@@ -1,7 +1,18 @@
 __author__ = 'dankerrigan'
 
+import os
+import sys
+
+for directory in ['/etc/graphite-web', '/etc/graphite']:
+    if os.path.isdir(directory):
+        graphite_path = directory
+        break
+
+sys.path.append(directory)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
+
 from graphite.dashboard.models import Dashboard
 
-
 if __name__ == '__main__':
-    pass
+    for obj in Dashboard.objects:
+        print obj
